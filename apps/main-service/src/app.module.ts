23d71407f854650eeb090798f7e4a4caadef97dd
Anton Lavrenchuk <anton@avrelinex.com>
2026-03-10 08:36:27 +0100

    add prisma to main service

--- apps/main-service/src/app.module.ts
+++ apps/main-service/src/app.module.ts
@@ -1,10 +1,12 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { HealthModule } from './health/health.module';
+import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { HealthModule } from "./health/health.module";
+import { PrismaModule } from "./prisma/prisma.module";
 
 @Module({
-  imports: [HealthModule],
+  imports: [ConfigModule.forRoot(), HealthModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
